695b776524ef2c5e62f749e455d8357c87d29de4
gparap <gparap@protonmail.com>
2022-01-22 21:51:28 +0200

    Draw the clock seconds pointer and start displaying the time

--- analog-clock/assets/js/analogClock.js
+++ analog-clock/assets/js/analogClock.js
@@ -11,76 +11,87 @@ var radius = canvas.height / 3.3;
 // set the origin at the center of the screen
 context2d.translate(canvas.width / 3, canvas.height / 3);
 
-// draw the circle of the clock
-context2d.arc(0, 0, radius, 0, 360);
-context2d.fillStyle = 'white';
-context2d.fill();
+// display the time
+setInterval(displayTime, 1000);
 
-// draw a gradient around the clock
-var gradient = context2d.createRadialGradient(0, 0, radius / 10, 0, 0, radius * 1.33);
-gradient.addColorStop(0, '#555555');
-gradient.addColorStop(0.5, 'white');
-gradient.addColorStop(1, '#555555');
-context2d.strokeStyle = gradient;
-context2d.lineWidth = radius / 10;
-context2d.stroke();
-
-// draw circle origin of the clock's pointers
-context2d.beginPath();
-context2d.arc(0, 0, radius / 10, 0, 360);
-context2d.fillStyle = '#555555';
-context2d.fill();
-
-// setup the clock's text settings
-context2d.font = radius / 10 + "px monospace";
-context2d.textAlign = "center";
-context2d.textBaseline = "middle";
-
-// draw the clock's numbers
-for (var i=1; i<13; i++) {
-    // rotation angle of the text
-    var angle = i * Math.PI / 6;
-    
-    // save context before translations
-    context2d.save();
-    
-    // rotate context based on the angle
-    context2d.rotate(angle);
-    
-    // offset from inner clock's circle
-    context2d.translate(0, -radius/1.175);
-    
-    // fix the rotation of the text before drawing the number
-    // and restore the rotation after drawing the number
-    context2d.rotate(-angle);
-    context2d.fillText(i.toString(), 0, 0);
-    context2d.rotate(angle);
-    
-    // restore context after translations
-    context2d.restore();
+function displayTime() {
+    context2d.clearRect(0, 0, canvas.width, canvas.height);
+    drawClockBackground();
+    drawClockPointers();    
 }
 
-// get the exact current time
-var time = new Date();
-var hours = time.getHours();
-var minutes = time.getMinutes();
-    minutes = minutes % 60;
-    
-// set the angle of the hours-pointer
-hours = hours % 12;
-hours = (hours * Math.PI / 6);
-    
-// draw the hours-pointer path
-drawPointer(hours, radius / 15, 2.25);
+function drawClockBackground() {
+    // draw the circle of the clock
+    context2d.arc(0, 0, radius, 0, 360);
+    context2d.fillStyle = 'white';
+    context2d.fill();
 
-// set the angle of the minutes-pointer
-minutes = minutes % 60;
-minutes = (minutes * Math.PI / 30);
-    
-// draw the hours-pointer path
-drawPointer(minutes, radius / 15, 1.33);
+    // draw circle origin of the clock's pointers
+    context2d.beginPath();
+    context2d.arc(0, 0, radius / 10, 0, 360);
+    context2d.fillStyle = '#555555';
+    context2d.fill();
 
-// Draws the clock pointer
+    // setup the clock's text settings
+    context2d.font = radius / 10 + "px monospace";
+    context2d.textAlign = "center";
+    context2d.textBaseline = "middle";
+
+    // draw the clock's numbers (1 - 12)
+    drawClockNumbers();
+}
+
+function drawClockNumbers() {
+    for (var i=1; i<13; i++) {
+        // rotation angle of the text
+        var angle = i * Math.PI / 6;
+
+        // save context before translations
+        context2d.save();
+
+        // rotate context based on the angle
+        context2d.rotate(angle);
+
+        // offset from inner clock's circle
+        context2d.translate(0, -radius/1.175);
+
+        // fix the rotation of the text before drawing the number
+        // and restore the rotation after drawing the number
+        context2d.rotate(-angle);
+        context2d.fillText(i.toString(), 0, 0);
+        context2d.rotate(angle);
+
+        // restore context after translations
+        context2d.restore();
+    }
+}
+
+function drawClockPointers() {
+    // get the exact current time
+    var time = new Date();
+    var hours = time.getHours();
+    var minutes = time.getMinutes();
+    var seconds = time.getSeconds();
+
+    // set the angle of the hours-pointer
+    hours = (hours * Math.PI / 6) + ((Math.PI / 6) /(minutes * Math.PI / 30));
+    
+    // draw the hours-pointer path
+    drawPointer(hours, radius / 15, 2.25);
+    
+    // set the angle of the minutes-pointer
+    minutes = (minutes * Math.PI / 30);
+    
+    // draw the hours-pointer path
+    drawPointer(minutes, radius / 25, 1.33);
+    
+    // set the angle of the seconds-pointer
+    seconds = (seconds * Math.PI / 30);
+    
+    // draw the seconds-pointer path
+    drawPointer(seconds, radius / 50, 1.25);
+}
+
 function drawPointer(radians, lineWidth, lineLength) {
     context2d.beginPath();
     
@@ -93,6 +104,7 @@ function drawPointer(radians, lineWidth, lineLength) {
     context2d.lineTo(0, -radius / lineLength);
     context2d.stroke();
     
-    // restore rotation
+    // restore rotation and closh path
     context2d.rotate(-radians);
+    context2d.closePath();
 }
