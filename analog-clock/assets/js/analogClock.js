8f199b810aa3770745581f144b6e72c7d8e6034c
gparap <gparap@protonmail.com>
2022-01-22 20:12:39 +0200

    Draw the clock hour & minutes pointers

--- analog-clock/assets/js/analogClock.js
+++ analog-clock/assets/js/analogClock.js
@@ -6,16 +6,13 @@ var canvas = document.getElementById('canvas');
 var context2d = canvas.getContext('2d');
 
 // set the half of the minimum dimension (height) as the clock's radius
-var radius = canvas.height / 3;
+var radius = canvas.height / 3.3;
 
 // set the origin at the center of the screen
 context2d.translate(canvas.width / 3, canvas.height / 3);
 
-// offset from origin's top
-var offsetTop = 50;
-
 // draw the circle of the clock
-context2d.arc(0, offsetTop, radius, 0, 360);
+context2d.arc(0, 0, radius, 0, 360);
 context2d.fillStyle = 'white';
 context2d.fill();
 
@@ -30,13 +27,13 @@ context2d.stroke();
 
 // draw circle origin of the clock's pointers
 context2d.beginPath();
-context2d.arc(0, offsetTop, radius / 10, 0, 360);
+context2d.arc(0, 0, radius / 10, 0, 360);
 context2d.fillStyle = '#555555';
 context2d.fill();
 
 // setup the clock's text settings
 context2d.font = radius / 10 + "px monospace";
-context2d.textAlign = "center"
+context2d.textAlign = "center";
 context2d.textBaseline = "middle";
 
 // draw the clock's numbers
@@ -56,9 +53,46 @@ for (var i=1; i<13; i++) {
     // fix the rotation of the text before drawing the number
     // and restore the rotation after drawing the number
     context2d.rotate(-angle);
-    context2d.fillText(i.toString(), 0, offsetTop);
+    context2d.fillText(i.toString(), 0, 0);
     context2d.rotate(angle);
     
     // restore context after translations
     context2d.restore();
+}
+
+// get the exact current time
+var time = new Date();
+var hours = time.getHours();
+var minutes = time.getMinutes();
+    minutes = minutes % 60;
+    
+// set the angle of the hours-pointer
+hours = hours % 12;
+hours = (hours * Math.PI / 6);
+    
+// draw the hours-pointer path
+drawPointer(hours, radius / 15, 2.25);
+
+// set the angle of the minutes-pointer
+minutes = minutes % 60;
+minutes = (minutes * Math.PI / 30);
+    
+// draw the hours-pointer path
+drawPointer(minutes, radius / 15, 1.33);
+
+// Draws the clock pointer
+function drawPointer(radians, lineWidth, lineLength) {
+    context2d.beginPath();
+    
+    // center on the inner circle of the clock
+    context2d.moveTo(0, 0);
+    
+    // draw the clock pointer based on the rotation 
+    context2d.rotate(radians);
+    context2d.lineWidth = lineWidth;
+    context2d.lineTo(0, -radius / lineLength);
+    context2d.stroke();
+    
+    // restore rotation
+    context2d.rotate(-radians);
 }
