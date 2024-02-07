390cfda3fb02919f635c379c8d2c36983b22aaa4
gparap <gparap@protonmail.com>
2024-02-07 19:16:35 +0200

    Fetch all recipes data from an API & Refactor code

--- recipes/assets/scripts/recipes.js
+++ recipes/assets/scripts/recipes.js
@@ -1,18 +1,40 @@
 // © 2024 gparap
 
-/* Handles the shortened display of all the recipes texts after page loading. */
+/* Keeper of all recipes data. */
+let recipes = null;
+
+/* Fetches all recipes data from an API. */
 function initRecipes() {
+    fetch('<<REPLACE-WITH-API>>')
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(`Error: ${response.statusText}`);
+            }
+            return response.json();
+        })
+        .then(data => {
+            recipes = data.recipes;
+            viewRecipes(data.recipes);
+            shortenRecipes();
+        })
+        .catch(error => {
+            console.error('Error:', error);
+        });
+}
+
+/* Handles the shortened display of all the recipes texts after page loading. */
+function shortenRecipes() {
     const recipesTexts = document.querySelectorAll('.card-text');
+    let i = 0;
     recipesTexts.forEach(element => {
         //remove extra spaces and line breaks
         //!!! when applying a code formatting
-        const formattedText = element.textContent.replace(/\s+/g, ' ').trim();
-
-        //keep the full text of the recipe
-        let recipe
+        const formattedText = recipes[i].description.replace(/\s+/g, ' ').trim();
 
         //give the element the shortend text
         element.innerHTML = getShortendText(formattedText);
+
+        i++;
     });
 }
 
@@ -48,6 +70,29 @@ function viewRecipe(id) {
 
     //update the modal with the recipe
     title.innerHTML = recipes[id - 1].title;
-    recipe.innerHTML = recipes[id - 1].recipe;
-    image.src = "assets/img/" + recipes[id - 1].image;
+    recipe.innerHTML = `<p>${recipes[id - 1].description}<br><br>${recipes[id - 1].recipe}</p>`;
+    image.src = recipes[id - 1].image;
 }
+
+/* Displays all recipes on the page. */
+const viewRecipes = (recipesData) => {
+    const container = document.getElementById('container-recipes');
+
+    //append recipe to container
+    recipesData.forEach((recipe) => {
+        container.innerHTML +=`
+            <div class="col-md-4">
+                <div class="card" id="${recipe.id}">
+                    <img class="card-img-top w-100 d-block" src="${recipe.image}">
+                    <div class="card-body">
+                        <h4 class="card-title">
+                            <p>${recipe.title}</p>
+                        </h4>
+                        <p class="card-text">${recipe.description}</p>
+                        <button onclick="viewRecipe(${recipe.id});" class="btn btn-dark" type="button"
+                            data-bs-toggle="modal" data-bs-target="#recipeModal">View Recipe</button>
+                    </div>
+                </div>
+            </div>`;
+    });
+};
